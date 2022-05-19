104f387485b918f5112326e94a5ab5d51c7bc139
Nikita Zemlyanushkin <coonseo.322@gmail.com>
2022-05-19 09:59:53 +0600

    added blog views and templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,9 +10,9 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
-    if request.method == 'POTS':
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -27,14 +27,16 @@ def register():
             try:
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password))
+                    (username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
-                return redirect(url_for("auyh.login"))
+                return redirect(url_for("auth.login"))
+
         flash(error)
+
     return render_template('auth/register.html')
 
 
